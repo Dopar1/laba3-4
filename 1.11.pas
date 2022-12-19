079d874ba22cd72824582166a2f6a1866c114507
Dopar1 <120112366+Dopar1@users.noreply.github.com>
2022-12-19 13:44:32 +0300

    Add files via upload

---
+++ 1.11.pas
@@ -0,0 +1,11 @@
+﻿var a,b,sum1,sum2,k:Longint;
+begin
+  a:=0;
+var a,b,c:integer;
+begin
+  writeln ('Введите число');
+  readln (a);
+  b:=1;
+  for c:= 1 to a do b:=b*c;
+  write (a,'!=',b); 
+end.
---
+++ 1.12.pas
@@ -0,0 +1,19 @@
+﻿var a,b,sum1,sum2,k:Longint;
+begin
+  a:=0;
+var a,b,i,c,d:integer;
+begin
+  writeln ('Введите диапазон');
+  readln (a,b);
+  writeln ('Сейчас мы посчитаем сумму четных числе и произведение нечетных чисел в диапазоне от ',a,' до ',b);
+  d:=1;
+  c:=0;
+ repeat
+   if a mod 2 = 0 then
+     c:=c+a else
+       d:=d*a;
+     a:=a+1;
+ until a=b ;
+ writeln ('Сумма четных - ',c);
+ writeln ('Произведение нечетных - ',d);
+end.
